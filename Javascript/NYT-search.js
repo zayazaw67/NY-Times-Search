c77dcc07c07a9d4a7e200073f946c713648c525f
Zaya <zayazaw67@gmail.com>
2019-09-16 14:03:23 -0500

    added submit  and clear functions and records input from submit

---
+++ Javascript/NYT-search.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+var searchTerm = "";
+var numberRecords = 0;
+var startYear = 0;
+var endYear = 0;
+
+$(".submit").on("click", function() {
+    searchTerm = $("#search").val().trim();
+
+    numberRecords = $("#record").val()
+
+
+    startYear = $('#startYear').val().trim();
+    endYear = $('#endYear').val().trim();
+
+    console.log(searchTerm);
+    console.log(numberRecords);
+
+})
+
+$(".clear").on("click", function() {
+    searchTerm = $("#search").val("");
+    numberRecords = 
+    startYear =  $("#startYear").val("");
+    endYear =  $("#endYear").val("");
+})
+
+});
